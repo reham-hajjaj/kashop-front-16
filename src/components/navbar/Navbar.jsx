38e69c2d1b23f7da1a1f22ed181690bde35ef585
Reham Hajjaj <rehamhajjaj0@gimal.com>
2026-06-24 23:05:34 +0300

    create component register

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import"./Navbar";
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { useForm } from 'react-hook-form';
+import { data } from 'react-router-dom';
+import axios from 'axios';
 export default function Register() {
+  const {register,handleSubmit} = useForm();
+  const RegisterFrom = async(data)=>{
+try{
+const response =await axios.post( '${import.meta.evn.VITE_BURL }/auth/Account/Register',data);
+console.log(response);
+}catch(Error){
+  console.log(Error);
+}
+  }
   return (
-    <div>Register</div>
+
+   <Box component="section" className="registerpage">
+    <Typography component ="h1"variant ="h2"> 
+Register
+    </Typography>
+    <Box component="form" sx ={{marginTop: 2 , display :"flex",flexDirection: "column", gap :2 }}>
+      
+      <TextField fullWidth {...register("userName")} label="userName" variant="outlined" />
+       <TextField fullWidth {...register("password")}  label="password" variant="outlined" />
+        <TextField fullWidth {...register("fullName")}  label="fullName" variant="outlined" />
+        <TextField fullWidth {...register("email")}  label="email" variant="outlined" />
+         <TextField fullWidth {...register("phoneNumber")}  label="phoneNumber" variant="outlined" />
+          
+  <button variant="contained" type="submit">
+  Register
+</button>
+      
+    </Box>
+   </Box>
   )
 }
