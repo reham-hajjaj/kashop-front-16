9f23a48ac30406d0a26c5995eafc1e3f03a8a991
Reham Hajjaj <rehamhajjaj0@gimal.com>
2026-07-17 18:19:32 +0300

    token  Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,61 @@
 import React from 'react'
 import"./Navbar";
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import useAuthStore from '../../store/useAuthStore';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Typography from "@mui/material/Typography";
+import Toolbar from '@mui/material/Toolbar';
 export default function Navbar() {
-  
+  const navigate =useNavigate();
+  const token =useAuthStore((state)=>state.token);
+  const logout = useAuthStore((state)=>state.logout);
+  const handleLogout =()=>{
+    logout();
+    navigate('/login');
+  }
+  console.log(token); 
   return (
-    <nav>
- <Link to="/">Home</Link>
- <Link to="/products">Products</Link>
- <Link to="/cart">Cart</Link>
- <Link to="/login">Login</Link>
- <Link to="/register">Register</Link>
+    <appBar postion="static">
+      <Toolbar >
+        <Typography variant= "h6" sx={{flexGrow:1 ,fontWeight:"bold"}}></Typography>
+        <Button color="inherit">
+          <Link to="/">Home</Link>
+        </Button>
+<Button color="inherit" >
+   <Link to="/products">Products</Link>
+</Button>
  
-    </nav>
+ {token?<>
+ <Button color="error" variant="contained"> 
+  <Link to="/login" component="button"onClick={handleLogout}>Logout</Link>
+ </Button >
+
+<Button  color="inherit">
+ <Link to="/cart">Cart</Link>
+</Button>
+  
+ </>:
+ <>
+ <Button >
+  <Link to="/login">Login</Link>
+ </Button>
+<Button variant="contained">
+   <Link to="/register">Register</Link>
+</Button>
+ 
+ </>
+
+ }
+      </Toolbar>
+
+ 
+    </appBar>
+ 
+ 
+ 
+    
   
 
   
