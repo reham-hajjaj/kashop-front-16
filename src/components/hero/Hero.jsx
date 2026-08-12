4b40ddafb38a5eda7c49482ea813dff200080da5
Reham Hajjaj <rehamhajjaj0@gimal.com>
2026-08-13 00:41:04 +0300

    Hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,22 +10,25 @@ export default function Hero() {
   const navigate =useNavigate ();
   const handleShopNow =()=>{navigate ("/shop")} ;
   return (
-    <Box sx={{backgroundColor:"#F5EDE3",minHeight:{xs:"350px",md:"450px"}, display:"flex",alignItems:"center", justifyContent:"center", mb:4,borderRadius:3}}>
+    <Box sx={{backgroundColor:"#F5E6D3",minHeight:{xs:"320px",md:"400px"}, display:"flex",alignItems:"center", justifyContent:"center", mb:4,borderRadius:3}}>
      <Container maxWidth="md">
-        <Grid container spacing={4} alignItems="center" justifyContent="center">
-            <Grid item xs={12} md={8}>
+        <Grid container spacing={4} alignItems="center" justifyContent="center" textAlign="center">
+            <Grid item xs={12}>
 <Typography  variant="h3" fontWeight="bold" sx={{color:"#f4511e"}}>
 Welcom to Our Store
 </Typography>
-<Typography variant="h2" sx={{mt:2,mb:3,color:"#222",}}>
+<Typography variant="h2" sx={{mt:2,mb:2,color:"#222", fontWeight:"bold"}}>
 Discover our amazing products
 </Typography>
-<Typography varint="h2" sx={{mt:2,mb:3,color:"#222",}}>
+<Typography varint="body1" sx={{mb:3,color:"#666",}}>
 Explore our collection and find high-quality products at amazing prices
 </Typography>
-<Button variant="contained"  size="large" onClick={handleShopNow } sx={{backgroundColor:"#f4511e",px:1.5,borderRadius:2}}>
+<Box sx={{ display:"flex", justifyContent:"center", width:"100%"}}>
+<Button variant="contained"  size="large" onClick={handleShopNow } sx={{backgroundColor:"#f4511e",py:1.5, px:4,borderRadius:2}}>
     Shop Now
 </Button>
+</Box>
+
             </Grid>
 
         </Grid>
