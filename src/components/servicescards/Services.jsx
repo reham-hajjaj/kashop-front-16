972841d70e6ff71dc75b4ba76e87e098e9a62675
Reham Hajjaj <rehamhajjaj0@gimal.com>
2026-08-16 23:31:36 +0300

    Services

--- src/components/servicescards/Services.jsx
+++ src/components/servicescards/Services.jsx
@@ -1,11 +1,15 @@
+import { Card, CardContent, Typography } from '@mui/material';
 import React from 'react'
 
 function Services({title, description,icon}) {
   return (
-    <Card sx={{height:'100%'}}>
-      <div>{icon}</div>
-       <div>{title}</div>
-        <div>{description}</div>
+    <Card sx={{height:'100%',textAlign:"center",borderRadius:3,boxShadow:3}}>
+      <CardContent>
+ <Typography variant='h4'>{icon}</Typography>
+       <Typography variant='h4'>{title}</Typography>
+        <Typography variant='body2'>{description}</Typography>
+      </CardContent>
+     
     </Card>
   );
 }
